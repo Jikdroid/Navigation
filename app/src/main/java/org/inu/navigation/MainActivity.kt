c94f81d4da82175c8fd49cc596e9eb42cd1ecc1d
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2022-05-25 19:36:24 +0900

    [#1] - feat: optionMenu, bottom nav, set actionBar title depending on label

--- app/src/main/java/org/inu/navigation/MainActivity.kt
+++ app/src/main/java/org/inu/navigation/MainActivity.kt
@@ -2,12 +2,19 @@ package org.inu.navigation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.widget.Toolbar
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.onNavDestinationSelected
 import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var appBarConfiguration : AppBarConfiguration
@@ -25,10 +32,40 @@ class MainActivity : AppCompatActivity() {
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
 
         setSupportActionBar(toolbar)
+
         setupActionBar(navController, appBarConfiguration)
+        setupBottomNavMenu(navController)
     }
 
+    //destination 의 label 에 따라 title 변경
     private fun setupActionBar(navController: NavController, appBarConfig : AppBarConfiguration) {
         setupActionBarWithNavController(navController,appBarConfig)
     }
+
+    // bottom nav 설정
+    private fun setupBottomNavMenu(navController: NavController) {
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_nav_view)
+        bottomNav?.setupWithNavController(navController)
+    }
+
+    // optionMenu 생성
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val retValue =  super.onCreateOptionsMenu(menu)
+        val navigationView = findViewById<NavigationView>(R.id.nav_view)
+        if (navigationView == null) {
+            menuInflater.inflate(R.menu.overflow_menu, menu)
+            return true
+        }
+        return retValue
+    }
+
+    // optionMenu 선택 설정
+    // Have the NavigationUI look for an action or destination matching the menu
+    // item id and navigate there if found.
+    // Otherwise, bubble up to the parent.
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return item.onNavDestinationSelected(findNavController(R.id.my_nav_host_fragment))
+//                || return super.onOptionsItemSelected(item)
+    }
+
 }
