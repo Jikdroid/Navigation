126ee6a995b86fc6585d3398a4624a8139d9e2c1
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2022-05-26 17:39:01 +0900

    [#1] - feat: deep_link from widget, weblink impl

---
+++ app/src/main/java/org/inu/navigation/DeepLinkAppWidgetProvider.kt
@@ -0,0 +1,27 @@
+package org.inu.navigation
+
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+import android.os.Bundle
+import android.widget.RemoteViews
+import androidx.navigation.NavDeepLinkBuilder
+
+class DeepLinkAppWidgetProvider:AppWidgetProvider() {
+    override fun onUpdate(context: Context, appWidgetManager: AppWidgetManager, appWidgetIds: IntArray?) {
+        val remoteViews = RemoteViews(context.packageName, R.layout.deep_link_appwidget)
+
+        val args = Bundle()
+        args.putString("myarg", "From Widget")
+
+        // construct and set a PendingIntent using DeepLinkBuilder
+        val pendingIntent  = NavDeepLinkBuilder(context)
+            .setGraph(R.navigation.mobile_navigation)
+            .setDestination(R.id.deeplink_dest)
+            .setArguments(args)
+            .createPendingIntent()
+        remoteViews.setOnClickPendingIntent(R.id.deep_link_button,pendingIntent)
+
+        appWidgetManager.updateAppWidget(appWidgetIds, remoteViews)
+    }
+}
