ae3dddb60c26c194db6f83f0883bf2e038c20a40
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2022-05-26 16:07:22 +0900

    [#1] - feat: FlowStepFragment.kt impl

--- app/src/main/java/org/inu/navigation/FlowStepFragment.kt
+++ app/src/main/java/org/inu/navigation/FlowStepFragment.kt
@@ -5,15 +5,29 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.navArgs
 
 
 class FlowStepFragment : Fragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_flow_step_one, container, false)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+//        setHasOptionsMenu(true)
+
+        val safeArgs: FlowStepFragmentArgs by navArgs()
+        val flowStepNumber = safeArgs.flowStepNumber
+
+        return when(flowStepNumber){
+            1 -> inflater.inflate(R.layout.fragment_flow_step_one, container, false)
+            else -> inflater.inflate(R.layout.fragment_flow_step_two, container, false)
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<View>(R.id.next_button).setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.next_action)
+        )
     }
 }
